171be015ab2d7044fb7cb26fa82e40d13571e9f1
Satyam Singh Virat <satyam23599@gmail.com>
2023-08-08 16:02:02 +0530

    router and controller added for home

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000;  // generally it's 80
 
+// use express router
+app.use('/', require('./routes/index'));
+
+
+
+
+
 app.listen(port, function(err){
     if(err){
         // console.log('error', err);
